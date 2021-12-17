6e14e77835d1b999955be00c3085e47a26da0935
Debarshi Banerjee <45908114+debarshibanerjee@users.noreply.github.com>
2021-12-17 15:59:00 +0100

    fixed basic-mpi-codes

--- self-tests/mpi/basic-mpi-codes/hello_world.F90
+++ self-tests/mpi/basic-mpi-codes/hello_world.F90
@@ -1,9 +1,10 @@
-PROGRAM hello
- INCLUDE ‘mpif.h‘
- INTEGER err
- CALL MPI_INIT(err)
- call MPI_COMM_RANK(MPI_COMM_WORLD,rank,ierr)
- call MPI_COMM_SIZE(MPI_COMM_WORLD,size,ierr)
- print *, 'I am ', rank, ' of ', size
- CALL MPI_FINALIZE(err)
-END
+program hello
+    implicit none
+    include 'mpif.h'
+    integer err,rank,size
+    call MPI_INIT(err)
+    call MPI_COMM_RANK(MPI_COMM_WORLD,rank,err)
+    call MPI_COMM_SIZE(MPI_COMM_WORLD,size,err)
+    print *, 'I am ', rank, ' of ', size
+    call MPI_FINALIZE(err)
+end program hello
--- self-tests/mpi/basic-mpi-codes/hello_world.c
+++ self-tests/mpi/basic-mpi-codes/hello_world.c
@@ -1,13 +1,12 @@
-// simple hello world program 
-#include <stdio.h>
+// simple hello world program
 #include <mpi.h>
+#include <stdio.h>
 
-int main (int argc, char * argv[])
-{
-  int rank, size;
-  MPI_Init( &argc, &argv );
-  MPI_Comm_rank( MPI_COMM_WORLD,&rank );
-  MPI_Comm_size( MPI_COMM_WORLD,&size );  
-  printf( "I am %d of %d\n", rank, size );
-  MPI_Finalize();
+int main(int argc, char* argv[]) {
+	int rank, size;
+	MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	printf("I am %d of %d\n", rank, size);
+	MPI_Finalize();
 }
